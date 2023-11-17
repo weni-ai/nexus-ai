a65f1da8e6515c47dabe81e65ccb9935e4ad185b
Mardone <mardone.dev@gmail.com>
2023-11-16 21:30:42 -0300

    Feat: Delete Intelligence usecase

--- nexus/usecases/intelligences/__init__.py
+++ nexus/usecases/intelligences/__init__.py
@@ -1,4 +1,5 @@
 from .list import ListIntelligencesUseCase
 from .create import CreateIntelligencesUseCase
 from .update import UpdateIntelligenceUseCase
+from .delete import DeleteIntelligenceUseCase
 from .get_by_uuid import get_by_uuid
