6a94e2660d6f7da2d4efb45ab1c7992004bf966b
Mardone <mardone.dev@gmail.com>
2023-11-22 18:03:58 -0300

    Feat: List usecase

--- nexus/usecases/intelligences/list.py
+++ nexus/usecases/intelligences/list.py
@@ -1,5 +1,6 @@
-from nexus.intelligences.models import Intelligence
+from nexus.intelligences.models import Intelligence, ContentBase
 from nexus.usecases import orgs
+from .get_by_uuid import get_by_intelligence_uuid
 
 
 class ListIntelligencesUseCase():
@@ -10,3 +11,13 @@ class ListIntelligencesUseCase():
     def get_org_intelligences(self, org_uuid: str):
         org = orgs.get_by_uuid(org_uuid)
         return Intelligence.objects.filter(org=org)
+
+
+class ListContentBaseUseCase():
+
+    def __init__(self):
+        pass
+
+    def get_intelligence_contentbases(self, intelligence_uuid: str):
+        intelligence = get_by_intelligence_uuid(intelligence_uuid)
+        return ContentBase.objects.filter(intelligence=intelligence)
--- nexus/usecases/intelligences/tests/test_list_intelligence.py
+++ nexus/usecases/intelligences/tests/test_list_intelligence.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
-from ..list import ListIntelligencesUseCase
+from ..list import ListIntelligencesUseCase, ListContentBaseUseCase
 
 from nexus.orgs.models import Org
 from nexus.users.models import User
-from nexus.intelligences.models import Intelligence
+from nexus.intelligences.models import Intelligence, ContentBase
 
 
 class TestListIntelligenceUseCase(TestCase):
@@ -27,3 +27,34 @@ class TestListIntelligenceUseCase(TestCase):
         use_case = ListIntelligencesUseCase()
         intelligences_list = use_case.get_org_intelligences(self.org.uuid)
         self.assertEqual(1, len(intelligences_list))
+
+
+class TestListContentBaseUseCase(TestCase):
+
+    def setUp(self):
+
+        self.user = User.objects.create(
+            email='test_org@user.com',
+            language='en'
+        )
+        self.org = Org.objects.create(
+            name='Test Org',
+            created_by=self.user,
+        )
+        self.intelligence = Intelligence.objects.create(
+            name='Test Intelligence',
+            created_by=self.user,
+            org=self.org
+        )
+        self.contentbase = ContentBase.objects.create(
+            intelligence=self.intelligence,
+            created_by=self.user,
+            title="title"
+        )
+
+    def test_count_contentbase_use_case(self):
+        use_case = ListContentBaseUseCase()
+        contentbase_list = use_case.get_intelligence_contentbases(
+            self.intelligence.uuid
+        )
+        self.assertEqual(1, len(contentbase_list))
