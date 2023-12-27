cc5ed360f156bc0e584cd3a52af9237ec38dffa4
Mardone <mardone.dev@gmail.com>
2023-12-27 17:36:20 -0300

    Add: Org filters usecase

---
+++ nexus/usecases/orgs/get_by_intelligence.py
@@ -0,0 +1,32 @@
+from nexus.orgs.models import Org
+
+
+class GetOrgByIntelligenceUseCase():
+
+    def get_org_by_intelligence_uuid(
+            self,
+            intelligence_uuid: str
+    ) -> Org:
+
+        org = Org.objects.get(
+            intelligences__uuid=intelligence_uuid
+        )
+        return org
+
+    def get_org_by_contentbase_uuid(
+            self,
+            contentbase_uuid: str
+    ) -> Org:
+        org = Org.objects.get(
+            intelligences__contentbases__uuid=contentbase_uuid
+        )
+        return org
+
+    def get_by_contentbasetext_uuid(
+            self,
+            contentbasetext_uuid: str
+    ) -> Org:
+        org = Org.objects.get(
+            intelligences__contentbases__contentbasetexts__uuid=contentbasetext_uuid
+        )
+        return org
