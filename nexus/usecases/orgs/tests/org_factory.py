9310f5616d4baa12f7b5f605ca97849909ab31f5
Mardone <mardone.dev@gmail.com>
2023-12-27 17:36:41 -0300

    Add: Exceptions and OrgAuth Factory

--- nexus/usecases/orgs/tests/org_factory.py
+++ nexus/usecases/orgs/tests/org_factory.py
@@ -1,6 +1,6 @@
 import factory
 
-from nexus.orgs.models import Org
+from nexus.orgs.models import Org, OrgAuth
 from nexus.usecases.users.tests.user_factory import UserFactory
 
 
@@ -10,3 +10,12 @@ class OrgFactory(factory.django.DjangoModelFactory):
 
     name = factory.Sequence(lambda n: 'test%d' % n)
     created_by = factory.SubFactory(UserFactory)
+
+
+class OrgAuthFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = OrgAuth
+
+    org = factory.SubFactory(OrgFactory)
+    user = factory.SubFactory(UserFactory)
+    role = 4
