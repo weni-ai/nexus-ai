6ff2c2048b294561b6088c23280e18c750c5ee7c
Mardone <mardone.dev@gmail.com>
2023-12-28 15:17:49 -0300

    Add: Send user email when recieving an api request

--- nexus/intelligences/api/views.py
+++ nexus/intelligences/api/views.py
@@ -26,31 +26,34 @@ class IntelligencesViewset(
     pagination_class = CustomCursorPagination
 
     def get_queryset(self):
+        user_email = self.request.user.email
         use_case = intelligences.ListIntelligencesUseCase()
         org_uuid = self.kwargs.get('org_uuid')
         use_case_list = use_case.get_org_intelligences(
-            org_uuid
+            org_uuid,
+            user_email=user_email
         )
 
         return use_case_list
 
     def retrieve(self, request, *args, **kwargs):
-
+        user_email = request.user.email
         intelligence_uuid = kwargs.get('intelligence_uuid')
         use_case = intelligences.RetrieveIntelligenceUseCase()
         intelligence = use_case.get_intelligence(
-            intelligence_uuid=intelligence_uuid
+            intelligence_uuid=intelligence_uuid,
+            user_email=user_email
         )
         serializer = IntelligenceSerializer(intelligence)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request, org_uuid=str):
+        user_email = request.user.email
         use_case = intelligences.CreateIntelligencesUseCase()
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        user_email = request.data.get("email")
         name = serializer.validated_data.get('name')
         description = serializer.validated_data.get('description')
 
@@ -67,12 +70,14 @@ class IntelligencesViewset(
         )
 
     def update(self, request):
+        user_email = request.user.email
         use_case = intelligences.UpdateIntelligenceUseCase()
 
         update_intelligence = use_case.update_intelligences(
             intelligence_uuid=request.data.get('intelligence_uuid'),
             name=request.data.get('name'),
-            description=request.data.get('description')
+            description=request.data.get('description'),
+            user_email=user_email
         )
 
         return Response(
@@ -81,12 +86,14 @@ class IntelligencesViewset(
         )
 
     def destroy(self, request):
+        user_email = request.user.email
         use_case = intelligences.DeleteIntelligenceUseCase()
 
         intelligence_uuid = request.data.get('intelligence_uuid')
 
         use_case.delete_intelligences(
-            intelligence_uuid=intelligence_uuid
+            intelligence_uuid=intelligence_uuid,
+            user_email=user_email
         )
 
         return Response(
@@ -102,31 +109,34 @@ class ContentBaseViewset(
     serializer_class = ContentBaseSerializer
 
     def get_queryset(self):
+        user_email = self.request.user.email
         use_case = intelligences.ListContentBaseUseCase()
         intelligence_uuid = self.kwargs.get('intelligence_uuid')
         use_case_list = use_case.get_intelligence_contentbases(
-            intelligence_uuid
+            intelligence_uuid,
+            user_email=user_email
         )
         return use_case_list
 
     def retrieve(self, request, *args, **kwargs):
-
+        user_email = request.user.email
         contentbase_uuid = kwargs.get('content_base_uuid')
         use_case = intelligences.RetrieveContentBaseUseCase()
         contentbase = use_case.get_contentbase(
-            contentbase_uuid=contentbase_uuid
+            contentbase_uuid=contentbase_uuid,
+            user_email=user_email
         )
         serializer = ContentBaseSerializer(contentbase)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request, intelligence_uuid=str):
+        user_email = request.user.email
         use_case = intelligences.CreateContentBaseUseCase()
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         title = serializer.validated_data.get('title')
-        user_email = request.data.get("email")
 
         contentbase = use_case.create_contentbase(
             intelligence_uuid=intelligence_uuid,
@@ -140,11 +150,13 @@ class ContentBaseViewset(
         )
 
     def update(self, request):
+        user_email = request.user.email
         use_case = intelligences.UpdateContentBaseUseCase()
 
         update_contentbase = use_case.update_contentbase(
             contentbase_uuid=request.data.get('contentbase_uuid'),
-            title=request.data.get('title')
+            title=request.data.get('title'),
+            user_email=user_email
         )
 
         return Response(
@@ -153,12 +165,14 @@ class ContentBaseViewset(
         )
 
     def destroy(self, request):
+        user_email = request.user.email
         use_case = intelligences.DeleteContentBaseUseCase()
 
         contentbase_uuid = request.data.get('contentbase_uuid')
 
         use_case.delete_contentbase(
-            contentbase_uuid=contentbase_uuid
+            contentbase_uuid=contentbase_uuid,
+            user_email=user_email
         )
 
         return Response(
@@ -174,31 +188,36 @@ class ContentBaseTextViewset(
     serializer_class = ContentBaseTextSerializer
 
     def get_queryset(self):
+        user_email = self.request.user.email
         use_case = intelligences.ListContentBaseTextUseCase()
         contentbase_uuid = self.kwargs.get('contentbase_uuid')
         use_case_list = use_case.get_contentbase_contentbasetexts(
-            contentbase_uuid
+            contentbase_uuid,
+            user_email=user_email
         )
         return use_case_list
 
     def retrieve(self, request, *args, **kwargs):
-
+        user_email = request.user.email
         contentbasetext_uuid = kwargs.get('content_base_text_uuid')
+
         use_case = intelligences.RetrieveContentBaseTextUseCase()
         contentbasetext = use_case.get_contentbasetext(
-            contentbasetext_uuid=contentbasetext_uuid
+            contentbasetext_uuid=contentbasetext_uuid,
+            user_email=user_email
         )
+
         serializer = ContentBaseTextSerializer(contentbasetext)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request, contentbase_uuid=str):
+        user_email = request.user.email
         use_case = intelligences.CreateContentBaseTextUseCase()
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         text = serializer.validated_data.get('text')
-        user_email = request.data.get("email")
 
         contentbasetext = use_case.create_contentbasetext(
             contentbase_uuid=contentbase_uuid,
@@ -212,11 +231,13 @@ class ContentBaseTextViewset(
         )
 
     def update(self, request):
+        user_email = request.user.email
         use_case = intelligences.UpdateContentBaseTextUseCase()
 
         update_contentbasetext = use_case.update_contentbasetext(
             contentbasetext_uuid=request.data.get('contentbasetext_uuid'),
-            text=request.data.get('text')
+            text=request.data.get('text'),
+            user_email=user_email
         )
 
         return Response(
@@ -225,12 +246,14 @@ class ContentBaseTextViewset(
         )
 
     def destroy(self, request):
+        user_email = request.user.email
         use_case = intelligences.DeleteContentBaseTextUseCase()
 
         contentbasetext_uuid = request.data.get('contentbasetext_uuid')
 
         use_case.delete_contentbasetext(
-            contentbasetext_uuid=contentbasetext_uuid
+            contentbasetext_uuid=contentbasetext_uuid,
+            user_email=user_email
         )
 
         return Response(
