f158d211ad06b62b67a75670fb43bd242a42e38a
Mardone <mardone.dev@gmail.com>
2023-12-28 12:02:48 -0300

    Add: Retrieve usecase unittest

--- nexus/usecases/intelligences/retrieve.py
+++ nexus/usecases/intelligences/retrieve.py
@@ -3,21 +3,61 @@ from .get_by_uuid import (
     get_by_contentbase_uuid,
     get_by_contentbasetext_uuid
 )
+from nexus.usecases import orgs, users
+from nexus.orgs import permissions
+from .exceptions import IntelligencePermissionDenied
 
 
 class RetrieveIntelligenceUseCase():
 
-    def get_intelligence(self, intelligence_uuid: str):
+    def get_intelligence(
+            self,
+            intelligence_uuid: str,
+            user_email: str
+    ):
+        user = users.get_by_email(user_email)
+        org_use_case = orgs.GetOrgByIntelligenceUseCase()
+        org = org_use_case.get_org_by_intelligence_uuid(intelligence_uuid)
+
+        has_permission = permissions.can_list_org_intelligences(user, org)
+        if not has_permission:
+            raise IntelligencePermissionDenied()
+
         return get_by_intelligence_uuid(intelligence_uuid)
 
 
 class RetrieveContentBaseUseCase():
 
-    def get_contentbase(self, contentbase_uuid: str):
+    def get_contentbase(
+            self,
+            contentbase_uuid: str,
+            user_email: str
+    ):
+        org_use_case = orgs.GetOrgByIntelligenceUseCase()
+        user = users.get_by_email(user_email)
+        org = org_use_case.get_org_by_contentbase_uuid(contentbase_uuid)
+
+        has_permission = permissions.can_list_content_bases(user, org)
+        if not has_permission:
+            raise IntelligencePermissionDenied()
+
         return get_by_contentbase_uuid(contentbase_uuid)
 
 
 class RetrieveContentBaseTextUseCase():
 
-    def get_contentbasetext(self, contentbasetext_uuid: str):
+    def get_contentbasetext(
+        self,
+        contentbasetext_uuid: str,
+        user_email: str
+    ):
+        org_use_case = orgs.GetOrgByIntelligenceUseCase()
+        user = users.get_by_email(user_email)
+        org = org_use_case.get_org_by_contentbasetext_uuid(
+            contentbasetext_uuid
+        )
+
+        has_permission = permissions.can_list_content_bases(user, org)
+        if not has_permission:
+            raise IntelligencePermissionDenied()
         return get_by_contentbasetext_uuid(contentbasetext_uuid)
---
+++ nexus/usecases/intelligences/tests/test_retrieve_intelligence.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+
+from nexus.intelligences.models import (
+    Intelligence,
+    ContentBase,
+    ContentBaseText
+)
+from ..retrieve import (
+    RetrieveContentBaseTextUseCase,
+    RetrieveContentBaseUseCase,
+    RetrieveIntelligenceUseCase
+)
+from .intelligence_factory import (
+    IntelligenceFactory,
+    ContentBaseFactory,
+    ContentBaseTextFactory
+)
+
+
+class TestRetrieveIntelligenceUseCase(TestCase):
+
+    def setUp(self):
+        self.intelligence = IntelligenceFactory()
+
+    def test_count_intelligence_use_case(self):
+        use_case = RetrieveIntelligenceUseCase()
+        intelligences_retrieve = use_case.get_intelligence(
+            intelligence_uuid=self.intelligence.uuid,
+            user_email=self.intelligence.created_by.email
+            )
+        self.assertIsNotNone(intelligences_retrieve)
+        self.assertIsInstance(intelligences_retrieve, Intelligence)
+
+
+class TestRetrieveContentBaseUseCase(TestCase):
+
+    def setUp(self):
+        self.contentbase = ContentBaseFactory()
+
+    def test_count_contentbase_use_case(self):
+        use_case = RetrieveContentBaseUseCase()
+        contentbase_retrieve = use_case.get_contentbase(
+            contentbase_uuid=self.contentbase.uuid,
+            user_email=self.contentbase.created_by.email
+        )
+        self.assertIsNotNone(contentbase_retrieve)
+        self.assertIsInstance(contentbase_retrieve, ContentBase)
+
+
+class TestRetrieveContentBaseTextUseCase(TestCase):
+
+    def setUp(self):
+        self.contentbasetext = ContentBaseTextFactory()
+
+    def test_count_contentbasetext_use_case(self):
+        use_case = RetrieveContentBaseTextUseCase()
+        contentbasetext_retrieve = use_case.get_contentbasetext(
+            contentbasetext_uuid=self.contentbasetext.uuid,
+            user_email=self.contentbasetext.created_by.email
+        )
+        self.assertIsNotNone(contentbasetext_retrieve)
+        self.assertIsInstance(contentbasetext_retrieve, ContentBaseText)
