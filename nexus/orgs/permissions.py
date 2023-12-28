8a86f97061bbb7803840b7916bd4f573b0017a15
Mardone <mardone.dev@gmail.com>
2023-12-28 12:08:25 -0300

    Add: Authorization to use update usecase

--- nexus/orgs/permissions.py
+++ nexus/orgs/permissions.py
@@ -53,6 +53,11 @@ def can_delete_content_bases(user: User, org: Org):
     return can_contribute(auth)
 
 
+def can_edit_content_bases(user: User, org: Org):
+    auth = get_user_auth(user, org)
+    return can_contribute(auth)
+
+
 def can_add_content_base_file(user: User, org: Org):
     auth = get_user_auth(user, org)
     return can_contribute(auth)
