f8c8c8e14b23e2c4e5edaf1cb057be7b06408ba5
barbosajackson <jbs@ic.ufal.br>
2024-01-16 11:20:10 -0300

    Revert "add task manager"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 export GUNICORN_APP=${GUNICORN_APP:-"nexus.wsgi"}
+export CELERY_APP=${CELERY_APP:-"app.wsgi:celery"}
+export GUNICORN_LOG_CONF=${GUNICORN_LOG_CONF:-"${APP_PATH}/docker/gunicorn/gunicorn-logging.conf"}
 export GUNICORN_CONF=${GUNICORN_CONF:-"${APP_PATH}/gunicorn.conf.py"}
-export GUNICORN_LOG_CONF=${GUNICORN_LOG_CONF:-"${APP_PATH}/gunicorn-logging.conf"}
 export LOG_LEVEL=${LOG_LEVEL:-"INFO"}
-export CELERY_APP=${CELERY_APP:-"nexus.celery"}
-export CELERY_MAX_WORKERS=${CELERY_MAX_WORKERS:-'6'}
-export HEALTHCHECK_TIMEOUT=${HEALTHCHECK_TIMEOUT:-"10"}
+#export GUNICORN_CONF=${GUNICORN_CONF:-"python:app.gunicorn"
 
 do_gosu(){
     user="$1"
@@ -37,41 +36,24 @@ do_gosu(){
 
 
 if [[ "start" == "$1" ]]; then
-    echo "starting server"
     do_gosu "${APP_USER}:${APP_GROUP}" python manage.py collectstatic --noinput
-    echo "collectstatic runned start gunicorn"
     do_gosu "${APP_USER}:${APP_GROUP}" exec gunicorn "${GUNICORN_APP}" \
-    #   --name="${APP_NAME}" \
-    #   --chdir="${APP_PATH}" \
-    #   --bind=0.0.0.0:8000 \
+      --name="${APP_NAME}" \
+      --chdir="${APP_PATH}" \
+      --bind=0.0.0.0:8000 \
       --log-config="${GUNICORN_LOG_CONF}" \
       -c "${GUNICORN_CONF}"
-elif [[ "celery-worker" == "$1" ]]; then
+elif [ "celery-worker" == "$1" ] ; then
     celery_queue="celery"
-    echo "celery worker"
     if [ "${2}" ] ; then
         celery_queue="${2}"
     fi
     do_gosu "${APP_USER}:${APP_GROUP}" exec celery \
-        -A "${CELERY_APP}" --workdir="${PROJECT_PATH}" worker \
+        -A "${CELERY_APP}" --workdir="${APP_PATH}" worker \
         -Q "${celery_queue}" \
         -O fair \
         -l "${LOG_LEVEL}" \
-        --autoscale=${CELERY_MAX_WORKERS},1
-elif [[ "healthcheck-celery-worker" == "$1" ]]; then
-    celery_queue="celery"
-    if [ "${2}" ] ; then
-        celery_queue="${2}"
-    fi
-    HEALTHCHECK_OUT=$(
-        do_gosu "${APP_USER}:${APP_GROUP}" celery -A "${CELERY_APP}" \
-            inspect ping \
-            -d "${celery_queue}@${HOSTNAME}" \
-            --timeout "${HEALTHCHECK_TIMEOUT}" 2>&1
-    )
-    echo "${HEALTHCHECK_OUT}"
-    grep -F -qs "${celery_queue}@${HOSTNAME}: OK" <<< "${HEALTHCHECK_OUT}" || exit 1
-    exit 0
+        --autoscale=4,1
 fi
 
 exec "$@"
--- nexus/task_manager/file_manager/file_manager.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC
-
-
-class FileManager(ABC):
-    pass
