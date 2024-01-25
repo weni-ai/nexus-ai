41559da2fe1b2d96e3a3d4aed4df2c618bd5d14a
Mardone <mardone.dev@gmail.com>
2024-01-25 14:59:14 -0300

    Add: usecase to search content base text

--- nexus/usecases/intelligences/get_by_uuid.py
+++ nexus/usecases/intelligences/get_by_uuid.py
@@ -39,10 +39,22 @@ def get_by_contentbasetext_uuid(contentbasetext_uuid: str) -> ContentBaseText:
     except ValidationError:
         raise ValidationError(message='Invalid UUID')
 
+
 def get_by_content_base_file_uuid(content_base_uuid: str) -> ContentBaseFile:
     try:
         return ContentBaseFile.objects.get(uuid=content_base_uuid)
     except ContentBaseFile.DoesNotExist:
         raise Exception(f"[ ContentBaseFile ] - ContentBaseFile with uuid `{content_base_uuid}` does not exists.")
     except Exception as exception:
-        raise(f"[ ContentBaseFile ] - ContentBaseFile error to get - error: `{exception}`")
+        raise (f"[ ContentBaseFile ] - ContentBaseFile error to get - error: `{exception}`")
+
+
+def get_contentbasetext_by_contentbase_uuid(
+        content_base_uuid: str
+) -> ContentBaseText:
+    try:
+        return ContentBaseText.objects.get(content_base__uuid=content_base_uuid)
+    except (ContentBaseText.DoesNotExist):
+        raise ContentBaseTextDoesNotExist()
+    except ValidationError:
+        raise ValidationError(message='Invalid UUID')
