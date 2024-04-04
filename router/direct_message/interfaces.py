cb1de8f5bd3afab05fefb7ecb53170c71d4107eb
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-04-04 18:37:30 -0300

    fix: Adjust DirectMessage interface params

--- router/direct_message/interfaces.py
+++ router/direct_message/interfaces.py
@@ -1,7 +1,8 @@
+from typing import List
 from abc import ABC, abstractmethod
 
 
 class DirectMessage(ABC):
     @abstractmethod
-    def send_direct_message(self, text: str, urn, project: str, user: str) -> None:
+    def send_direct_message(self, text: str, urns: List, project_uuid: str, user: str) -> None:
         pass
