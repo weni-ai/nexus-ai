03bd1f7e7fa663d6f975e8bc2562e6627b61cf46
Mardone <mardone.dev@gmail.com>
2024-04-05 14:46:34 -0300

    Add send message

--- nexus/usecases/projects/update.py
+++ nexus/usecases/projects/update.py
@@ -1,8 +1,32 @@
+from nexus.event_driven.publisher.rabbitmq_publisher import RabbitMQPublisher
 from nexus.projects.models import Project
 from nexus.usecases import users, intelligences
 from nexus.orgs import permissions
 from .dto import UpdateProjectDTO
 from .get_by_uuid import get_project_by_uuid
+
+
+def update_message(UpdateProjectDTO: UpdateProjectDTO):
+
+    publisher = RabbitMQPublisher()
+
+    action = "UPDATE"
+    entity = "NEXUS"
+    user = UpdateProjectDTO.user_email
+    project_uuid = UpdateProjectDTO.uuid
+
+    message = {
+        "action": action,
+        "entity": entity,
+        "user": user,
+        "project_uuid": project_uuid,
+    }
+
+    publisher.send_message(
+        body=message,
+        exchange="recent-activities.topic",
+        routing_key="brain_status"
+    )
 
 
 def update_project(
