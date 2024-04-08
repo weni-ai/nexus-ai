de86bd120acf302d5b1164c488ed9078f48696cd
Mardone <mardone.dev@gmail.com>
2024-04-08 17:34:45 -0300

    Add: New action field

--- nexus/actions/models.py
+++ nexus/actions/models.py
@@ -8,4 +8,5 @@ class Flow(models.Model):
     name = models.CharField(max_length=255)
     prompt = models.TextField()
     fallback = models.BooleanField(default=False)
+    product_handler = models.BooleanField(default=False)
     content_base = models.ForeignKey(ContentBase, on_delete=models.CASCADE, related_name="flows")
