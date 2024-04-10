1a9ce728c63b87a7c76222d2d6c8cdb3ec329d63
Alisson <alissonfilipe937@gmail.com>
2024-04-09 22:58:17 -0300

    add method to call wenigpt fine tunning model, set as default

--- router/indexer.py
+++ router/indexer.py
@@ -5,5 +5,5 @@ def get_chunks(indexer, text: str, content_base_uuid: str) -> List[str]:
     client = indexer
     response = client.search_data(content_base_uuid=content_base_uuid, text=text)
     if response.get("status") == 200:
-        texts_chunks: List[str] = response.get("data")
+        texts_chunks: List[str] = response.get("data").get("response")
         return texts_chunks
