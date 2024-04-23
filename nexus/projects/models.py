47bbcba78e38c27d702bc86614d696428af3d913
Mardone <mardone.dev@gmail.com>
2024-04-23 16:22:51 -0300

    Create: Models and migration

--- nexus/projects/models.py
+++ nexus/projects/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+from uuid import uuid4
+from enum import Enum
+
 from nexus.db.models import BaseModel, SoftDeleteModel
 from nexus.orgs.models import Org
+from nexus.users.models import User
 
 
 class TemplateType(models.Model):
@@ -29,3 +33,39 @@ class Project(BaseModel, SoftDeleteModel):
 
     def __str__(self):
         return f'{self.uuid} - Project: {self.name} - Org: {self.org.name}'
+
+
+class ProjectAuthorizationRole(Enum):
+    NOT_SETTED, VIEWER, CONTRIBUTOR, MODERATOR, SUPPORT, CHAT_USER = list(range(6))
+
+    @classmethod
+    def has_value(cls, value: int):
+        return value in cls._value2member_map_
+
+
+class ProjectAuth(models.Model):
+    class Meta:
+        unique_together = ['user', 'project']
+
+    ROLE_CHOICES = [
+        (ProjectAuthorizationRole.NOT_SETTED.value, 'not set'),
+        (ProjectAuthorizationRole.VIEWER.value, 'viewer'),
+        (ProjectAuthorizationRole.CONTRIBUTOR.value, 'contributor'),
+        (ProjectAuthorizationRole.MODERATOR.value, 'moderator'),
+        (ProjectAuthorizationRole.SUPPORT.value, 'support'),
+        (ProjectAuthorizationRole.CHAT_USER.value, 'chat user'),
+    ]
+
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name='authorizations'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='project_authorizations',
+    )
+    role = models.PositiveIntegerField(choices=ROLE_CHOICES)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.project} - {self.role}'
