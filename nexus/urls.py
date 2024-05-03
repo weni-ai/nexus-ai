3f6f8b8318ae373dc23f2d327d1c28a2fd01bfdb
Mardone <mardone.dev@gmail.com>
2024-05-03 14:16:44 -0300

    Test sentry

--- nexus/urls.py
+++ nexus/urls.py
@@ -10,6 +10,10 @@ from nexus.actions.api.routers import urlpatterns as actions_routes
 from nexus.projects.api.routers import urlpatterns as projects_routes
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
@@ -28,6 +32,7 @@ url_api += projects_routes
 
 urlpatterns = [
     path("", schema_view.with_ui("redoc")),
+    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(url_api)),
