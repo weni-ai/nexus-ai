773240447d89406127005f433907ac334ba09fe9
Alisson <alissonfilipe937@gmail.com>
2024-05-06 17:48:42 -0300

    remove env var to gen_env.py

--- contrib/gen_env.py
+++ contrib/gen_env.py
@@ -85,12 +85,6 @@ def generate_env():
         "DEFAULT_AGENT_ROLE": "",
         "DEFAULT_AGENT_PERSONALITY": "",
         "DEFAULT_AGENT_GOAL": "",
-        "EDA_BROKER_HOST": "",
-        "EDA_VIRTUAL_HOST": "",
-        "EDA_BROKER_PORT": "",
-        "EDA_BROKER_USER": "",
-        "EDA_BROKER_PASSWORD": "",
-        "EDA_WAIT_TIME_RETRY": "",
         "USE_EDA": True,
     }
 
