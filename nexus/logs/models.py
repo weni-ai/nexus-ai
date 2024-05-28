4d46c8a7d389534e8f2c82c56ca20aad8e5fec0d
Mardone <mardone.dev@gmail.com>
2024-05-28 10:25:19 -0300

    Add RecentActivity model

--- nexus/logs/models.py
+++ nexus/logs/models.py
@@ -3,8 +3,9 @@ from uuid import uuid4
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
+from nexus.users.models import User
 from nexus.projects.models import Project
-from nexus.intelligences.models import ContentBase
+from nexus.intelligences.models import ContentBase, Intelligence
 
 
 class Message(models.Model):
@@ -40,3 +41,25 @@ class MessageLog(models.Model):
 
     def __str__(self) -> str:
         return f"{self.message}"
+
+
+class RecentActivities(models.Model):
+
+    ACTION_CHOICES = (
+        ("C", "created"),
+        ("U", "updated"),
+        ("D", "deleted")
+    )
+
+    action_model = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid4)
+    action_details = models.JSONField(null=True, blank=True)
+    action_type = models.CharField(max_length=1, choices=ACTION_CHOICES)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    intelligence = models.ForeignKey(Intelligence, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.uuid} - {self.action_type}"
