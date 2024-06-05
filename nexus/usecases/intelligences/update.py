216013c6e342be5f81bc4d37bdd979b53fd757fc
Mardone <mardone.dev@gmail.com>
2024-06-05 17:48:45 -0300

    Fix: Flake8

--- nexus/usecases/intelligences/update.py
+++ nexus/usecases/intelligences/update.py
@@ -1,7 +1,6 @@
 from .get_by_uuid import (
     get_by_intelligence_uuid,
     get_by_contentbase_uuid,
-    get_by_contentbasetext_uuid,
     get_by_content_base_file_uuid,
     get_llm_by_project_uuid,
 )
