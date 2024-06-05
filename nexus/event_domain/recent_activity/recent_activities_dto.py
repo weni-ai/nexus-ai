405277ebb4d1de08f5e4d9e79d9ddd8a6cec2ea9
Mardone <mardone.dev@gmail.com>
2024-06-05 15:45:52 -0300

    Update: Change recent activity folder location

--- nexus/event_domain/recent_activity/recent_activities_dto.py
+++ nexus/event_domain/recent_activity/recent_activities_dto.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from nexus.intelligences.models import Intelligence
 from nexus.projects.models import Project
 from nexus.users.models import User
+from nexus.orgs.models import Org
 
 
 @dataclass
@@ -11,3 +12,12 @@ class CreateRecentActivityDTO:
     created_by: User
     intelligence: Intelligence
     action_details: dict = None
+
+
+@dataclass
+class RecentActivitiesDTO:
+    org: Org
+    user: User
+    entity_name: str
+    action: str
+    entity: str = "NEXUS"
--- nexus/usecases/event_driven/__init__.py
+++
@@ -1,2 +0,0 @@
-from .recent_activities import intelligence_activity_message
-from .publishers_dto import RecentActivitiesDTO
