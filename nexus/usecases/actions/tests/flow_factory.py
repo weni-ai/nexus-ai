3b33afbdfb91b1f525025228964dc054843d20d9
Mardone <mardone.dev@gmail.com>
2024-06-17 17:53:16 -0300

    Classification tests

--- nexus/usecases/actions/tests/flow_factory.py
+++ nexus/usecases/actions/tests/flow_factory.py
@@ -4,7 +4,6 @@ from uuid import uuid4
 
 from nexus.actions.models import Flow
 from nexus.usecases.intelligences.tests.intelligence_factory import ContentBaseFactory
-from nexus.usecases.users.tests.user_factory import UserFactory
 
 
 class FlowFactory(factory.django.DjangoModelFactory):
@@ -15,9 +14,7 @@ class FlowFactory(factory.django.DjangoModelFactory):
     uuid = str(uuid4().hex)
     name = factory.Sequence(lambda n: 'test%d' % n)
     prompt = factory.Sequence(lambda n: 'test%d' % n)
-    created_by = factory.SubFactory(UserFactory)
     content_base = factory.SubFactory(
         ContentBaseFactory,
-        created_by=factory.SelfAttribute('..created_by')
     )
     fallback = False
