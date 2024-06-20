1f9d0903b21968a040655e200f12bccafc7ed4a8
Mardone <mardone.dev@gmail.com>
2024-06-20 16:48:46 -0300

    Add: Custom authentication middleware

--- nexus/settings.py
+++ nexus/settings.py
@@ -96,6 +96,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'nexus.logs.middleware.PrometheusAuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'nexus.urls'
