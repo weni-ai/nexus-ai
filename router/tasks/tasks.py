288287576345c87a71a3bc764169064288a56e35
Mardone <mardone.dev@gmail.com>
2024-06-26 16:57:31 -0300

    Fix: Routing language

--- router/tasks/tasks.py
+++ router/tasks/tasks.py
@@ -13,6 +13,7 @@ from nexus.usecases.logs.create import CreateLogUsecase
 
 from router.route import route
 from router.classifiers.zeroshot import ZeroshotClassifier
+from router.classifiers.chatgpt_function import ChatGPT_Function_Classifier
 from router.classifiers import classify
 from router.clients.flows.http.flow_start import FlowStartHTTPClient
 from router.clients.flows.http.send_message import SendMessageHTTPClient
@@ -62,7 +63,22 @@ def start_route(message: Dict) -> bool:
             language=llm_model.setup.get("language", settings.WENIGPT_DEFAULT_LANGUAGE),
         )
 
-        classification: str = classify(ZeroshotClassifier(chatbot_goal=agent.goal), message.text, flows, llm_config.language)
+        if llm_config.model.lower() == "chatgpt":
+            classifier = ChatGPT_Function_Classifier(
+                api_key=llm_config.token,
+                chatgpt_model=llm_config.model_version,
+            )
+        else:
+            classifier = ZeroshotClassifier(
+                chatbot_goal=agent.goal
+            )
+
+        classification = classify(
+            classifier=classifier,
+            message=message.text,
+            flows=flows,
+            language=llm_config.language
+        )
 
         print(f"[+ Classification: {classification} +]")
 
