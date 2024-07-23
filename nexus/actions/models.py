6943f19ca79c21646235b5336d2c4764d71189c6
zMardone <mardone.dev@gmail.com>
2024-07-23 08:50:49 -0300

    Name script

--- nexus/actions/models.py
+++ nexus/actions/models.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 from django.db import models
 
 from nexus.intelligences.models import ContentBase
@@ -9,3 +12,45 @@ class Flow(models.Model):
     prompt = models.TextField()
     fallback = models.BooleanField(default=False)
     content_base = models.ForeignKey(ContentBase, on_delete=models.CASCADE, related_name="flows")
+
+    def name_generator(
+        self,
+        prompt:str,
+        goal:str
+    ):
+        # Contexto da classe: {context}
+
+        # Requisitos: 
+        instruction = []
+        instruction.append(f"O contexto da classe faz parte desse objetivo de chatbot: {goal}")
+        instruction.append("Responda sempre no formato: ```json\n{{\"class\": \"Nome da Classe\"}}\n```")
+        instruction.append("O nome da classe pode ser composto e ter espaços, mas deve ser curto.")
+
+        payload = json.dumps({
+            "messages": [
+                {
+                    "role": "user",
+                    "content": generate_prompt(one_shot_chatbot_goal, one_shot_context)
+                },
+                {
+                    "role": "assistant",
+                    "content": "```json\n{\"class\": \"Consultar Pedido\"}\n```"
+                },
+                {
+                    "role": "user",
+                    "content": generate_prompt(chatbot_goal, context)
+                }
+            ],
+            "model": "llama3-70b-8192",
+            "response_format": {
+                "type": "json_object"
+            }
+        })
+
+        headers = {
+        'Authorization': 'Bearer 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',
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.json()
