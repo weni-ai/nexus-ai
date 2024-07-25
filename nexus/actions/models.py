438be748fb8d953dea9df73553df3322e8f79b69
Mardone <mardone.dev@gmail.com>
2024-07-25 17:53:51 -0300

    Add generate name request

--- nexus/actions/models.py
+++ nexus/actions/models.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 from django.db import models
+from django.conf import settings
 
 from nexus.intelligences.models import ContentBase
 
@@ -13,24 +14,34 @@ class Flow(models.Model):
     fallback = models.BooleanField(default=False)
     content_base = models.ForeignKey(ContentBase, on_delete=models.CASCADE, related_name="flows")
 
-    def name_generator(
-        self,
-        prompt:str,
-        goal:str
-    ):
-        # Contexto da classe: {context}
+    def generate_prompt(chatbot_goal, context):
+        return f"""
+            A partir dos dados fornecidos, gere um nome de classe que melhor represente o Contexto da classe passado e os requisitos informados.
+            # Contexto da classe: {context}
+            # Requisitos: 
+            - O contexto da classe faz parte desse objetivo de chatbot: {chatbot_goal}
+            - Responda sempre no formato: ```json\n{{\"class\": \"Nome da Classe\"}}\n```
+            - O nome da classe pode ser composto e ter espaços, mas deve ser curto.
+            """
 
-        # Requisitos: 
-        instruction = []
-        instruction.append(f"O contexto da classe faz parte desse objetivo de chatbot: {goal}")
-        instruction.append("Responda sempre no formato: ```json\n{{\"class\": \"Nome da Classe\"}}\n```")
-        instruction.append("O nome da classe pode ser composto e ter espaços, mas deve ser curto.")
+    def generate_action_name(self, chatbot_goal, context):
 
+        # One shot examples:
+        one_shot_context = "Quando o usuário desejar APENAS consultar informações sobre o seu pedido."
+        one_shot_chatbot_goal = "Atendimento ao cliente da Weni, com foco em tirar dúvidas sobre o processo de compra, políticas de troca e devolução."
+
+        brain_goal = chatbot_goal
+        brain_context = context
+
+        url = "https://api.groq.com/openai/v1/chat/completions"
         payload = json.dumps({
             "messages": [
                 {
                     "role": "user",
-                    "content": generate_prompt(one_shot_chatbot_goal, one_shot_context)
+                    "content": self.generate_prompt(
+                        chatbot_goal=one_shot_chatbot_goal,
+                        context=one_shot_context
+                    )
                 },
                 {
                     "role": "assistant",
@@ -38,19 +49,22 @@ class Flow(models.Model):
                 },
                 {
                     "role": "user",
-                    "content": generate_prompt(chatbot_goal, context)
+                    "content": self.generate_prompt(
+                        chatbot_goal=brain_goal,
+                        context=brain_context
+                    )
                 }
             ],
-            "model": "llama3-70b-8192",
+            "model": settings.ACTION_GENERATE_NAME_MODEL,
             "response_format": {
                 "type": "json_object"
             }
         })
 
         headers = {
-        'Authorization': 'Bearer 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',
-        'Content-Type': 'application/json'
+            'Authorization': settings.ACTION_GENERATE_NAME_MODEL_AUTHORIZATION,
+            'Content-Type': 'application/json'
         }
-
+        # TODO - Add error handling, API and TestCase
         response = requests.request("POST", url, headers=headers, data=payload)
         return response.json()
