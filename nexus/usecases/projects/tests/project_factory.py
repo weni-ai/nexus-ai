5bd81ff0f1fd49da4a398711487aa78f060efff6
Mardone <mardone.dev@gmail.com>
2024-08-01 10:43:13 -0300

    Add feature version factory

--- nexus/usecases/projects/tests/project_factory.py
+++ nexus/usecases/projects/tests/project_factory.py
@@ -1,9 +1,19 @@
 import factory
 
-from nexus.projects.models import Project, ProjectAuth, ProjectAuthorizationRole
+from uuid import uuid4
+from faker import Faker
+
+from nexus.projects.models import (
+    Project,
+    ProjectAuth,
+    ProjectAuthorizationRole,
+    FeatureVersion
+)
 from nexus.usecases.orgs.tests.org_factory import OrgFactory
 from nexus.usecases.users.tests.user_factory import UserFactory
 
+
+fake = Faker()
 
 class ProjectFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -33,3 +43,36 @@ class ProjectAuthFactory(factory.django.DjangoModelFactory):
     project = factory.SubFactory(ProjectFactory)
     user = factory.SubFactory(UserFactory)
     role = ProjectAuthorizationRole.MODERATOR.value
+
+
+class FeatureVersionFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = FeatureVersion
+
+    uuid = uuid4().hex
+    setup = {
+        "agent": {
+            "name": fake.name(),
+            "role": fake.job(),
+            "personality": fake.word(
+                ext_word_list=[
+                    "Amigável",
+                    "Cooperativo",
+                    "Extrovertido",
+                    "Generoso"
+                ]
+            ),
+            "goal": "tirar duvidas sobre agentes inteligentes",
+        },
+        "instructions": [
+            "não falar palavrão",
+            "não fazer piada"
+        ],
+        "actions": [
+            {
+                "name": "atendimento humano",
+                "description": "Caso o usuário queira falar com um atendente humano"
+            }
+        ]
+    }
