d81c96d1d8cb56112090ab7a322af978895caaf8
Mardone <mardone.dev@gmail.com>
2024-08-01 12:10:31 -0300

    Add: Update consumer logic

--- nexus/projects/consumers/feature_version_consumer.py
+++ nexus/projects/consumers/feature_version_consumer.py
@@ -1,10 +1,13 @@
 import amqp
 from sentry_sdk import capture_exception
 
+from nexus.projects.models import FeatureVersion
+
 from nexus.event_driven.parsers import JSONParser
 from nexus.event_driven.consumer.consumers import EDAConsumer
 
 from nexus.usecases.projects.create import CreateFeatureVersionUseCase
+from nexus.usecases.projects.update import UpdateFeatureVersionUseCase
 
 
 class FeatureVersionConsumer(EDAConsumer):
@@ -13,15 +16,24 @@ class FeatureVersionConsumer(EDAConsumer):
         print(f"[FeatureVersion] - Consuming a message. Body: {message.body}")
         try:
             body = JSONParser.parse(message.body)
-            usecase = CreateFeatureVersionUseCase()
+            feature_version = FeatureVersion.objects.filter(uuid=body.get("uuid"))
 
-            created = usecase.create_feature_version(consumer_msg=body)
-            if created:
+            if feature_version.exists():
+                usecase = UpdateFeatureVersionUseCase()
+                usecase.update_feature_version(consumer_msg=body)
+
                 message.channel.basic_ack(message.delivery_tag)
-                print(f"[FeatureVersionConsumer] - FeatureVersion created: {body.uuid}")
+                print(f"[FeatureVersionConsumer] - FeatureVersion updated: {body.uuid}")
             else:
-                message.channel.basic_reject(message.delivery_tag, requeue=False)
-                print(f"[FeatureVersionConsumer] - Message rejected by: {body.uuid}")
+                usecase = CreateFeatureVersionUseCase()
+                created = usecase.create_feature_version(consumer_msg=body)
+
+                if created:
+                    message.channel.basic_ack(message.delivery_tag)
+                    print(f"[FeatureVersionConsumer] - FeatureVersion created: {body.uuid}")
+                else:
+                    message.channel.basic_reject(message.delivery_tag, requeue=False)
+                    print(f"[FeatureVersionConsumer] - Message rejected by: {body.uuid}")
 
         except Exception as exception:
             capture_exception(exception)
