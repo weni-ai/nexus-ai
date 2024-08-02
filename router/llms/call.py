47e8904f35634acdcb82b45e71cbc07f13199015
Mardone <mardone.dev@gmail.com>
2024-08-02 15:38:29 -0300

    Add: Base reflection call

--- router/llms/call.py
+++ router/llms/call.py
@@ -8,7 +8,10 @@ from router.entities import (
     Message,
 )
 
+from django.conf import settings
+
 from nexus.intelligences.llms.client import LLMClient
+from nexus.intelligences.llms.wenigpt import WeniGPTClient
 
 
 class Indexer:
@@ -35,5 +38,23 @@ def call_llm(
     )
 
     gpt_message = response.get("answers")[0].get("text")
+
+    if settings.REFLECTION_ENABLED:
+
+        if llm_model != WeniGPTClient:
+            return gpt_message
+
+        llm_model = WeniGPTClient(model_version=settings.REFLECTION_MODEL)
+        response = llm_model.request_gpt(
+            instructions=instructions,
+            chunks=chunks,
+            agent=agent.__dict__,
+            question=message.text,
+            llm_config=llm_config,
+            last_messages=last_messages,
+            reflection=True
+        )
+
+        gpt_message = response.get("answers")[0].get("text")
 
     return gpt_message
