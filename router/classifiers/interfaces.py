f96e2f54e1586aae7397043735058bbcb63be7a3
Mardone <mardone.dev@gmail.com>
2024-08-07 11:49:19 -0300

    Add: Base reflection interface

--- router/classifiers/interfaces.py
+++ router/classifiers/interfaces.py
@@ -14,3 +14,32 @@ class OpenAIClientInterface(ABC):
     @abstractmethod
     def chat_completions_create(self, model, messages, tools, tool_choice):
         pass  # pragma: no cover
+
+
+class ModelVersionReflection(ABC):
+
+    @abstractmethod
+    def request_reflection(
+        self,
+        prompt: str,
+    ):
+        pass
+
+    @abstractmethod
+    def format_prompt(self) -> str:
+        pass
+
+    @abstractmethod
+    def basic_reflection_strategy(
+        self,
+        prompt: str
+    ) -> str:
+        pass
+
+    @abstractmethod
+    def reflect(
+        self,
+        message_to_reflect: str
+    ) -> str:
+        # Should return only a strig with the reflected message
+        pass
