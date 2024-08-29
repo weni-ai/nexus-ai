793b0986e889a5277e29a6e525acc719aa75aecc
Mardone <mardone.dev@gmail.com>
2024-08-29 17:57:35 -0300

    Fix: routing error

--- nexus/actions/api/routers.py
+++ nexus/actions/api/routers.py
@@ -12,11 +12,14 @@ from .views import (
 flows_router = DefaultRouter()
 
 flows_router.register(r'', FlowsViewset, basename='flows')
-flows_router.register(r'template-action', TemplateActionView, basename='template-action')
+
+template_action_router = DefaultRouter()
+template_action_router.register(r'template-action', TemplateActionView, basename='template-action')
 
 urlpatterns = [
     path('<project_uuid>/flows/', include(flows_router.urls)),
     path('<project_uuid>/search-flows/', SearchFlowView.as_view()),
     path('<project_uuid>/preview/', MessagePreviewView.as_view()),
     path('<project_uuid>/generate-action-name/', GenerateActionNameView.as_view()),
+    path('<project_uuid>/', include(template_action_router.urls))
 ]
