9dbf0fef86178e782957a60b5bc193ac9d4a7352
Alisson <alissonfilipe937@gmail.com>
2024-09-25 17:34:12 -0300

    change ZeroshotClient ZEROSHOT_TOKEN to BOTHUB_ZEROSHOT_TOKEN

--- router/clients/zeroshot.py
+++ router/clients/zeroshot.py
@@ -23,7 +23,7 @@ class ZeroshotClient:
     def __init__(self, prompt: str) -> None:
         self.base_url = os.environ.get("ZEROSHOT_URL")
         self.prompt = prompt
-        self.token = os.environ.get("ZEROSHOT_TOKEN")
+        self.token = os.environ.get("BOTHUB_ZEROSHOT_TOKEN")
 
     def fast_predict(self, message: str, actions: List[Dict], language: str = "por"):
         url = f"{self.base_url}/v2/repository/nlp/zeroshot/zeroshot-fast-predict"
