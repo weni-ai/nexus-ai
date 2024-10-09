20eb8b652d5c042f95fd63b51c9d1da5730ac688
Alisson <alissonfilipe937@gmail.com>
2024-10-09 16:30:47 -0300

    remove duplicated delete method call

--- nexus/intelligences/api/views.py
+++ nexus/intelligences/api/views.py
@@ -606,8 +606,6 @@ class ContentBaseFileViewset(ModelViewSet):
             user=self.request.user
         )
 
-        return super().destroy(request, *args, **kwargs)
-
 
 class ContentBaseLinkViewset(ModelViewSet):
 
