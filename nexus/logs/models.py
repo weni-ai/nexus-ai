780e9890d0e3892bddc8314927fdad1d109d9555
Mardone <mardone.dev@gmail.com>
2024-10-15 16:03:09 -0300

    Add: New log field

--- nexus/logs/models.py
+++ nexus/logs/models.py
@@ -19,7 +19,8 @@ class Message(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4)
     text = models.TextField()
     contact_urn = models.CharField(max_length=255)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='P')
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, default='P')
     exception = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -32,12 +33,14 @@ class MessageLog(models.Model):
     chunks = ArrayField(models.TextField(), null=True)
     prompt = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
-    content_base = models.ForeignKey(ContentBase, on_delete=models.CASCADE, null=True)
+    content_base = models.ForeignKey(
+        ContentBase, on_delete=models.CASCADE, null=True)
     classification = models.CharField(max_length=255, null=True)
     llm_model = models.CharField(max_length=255, null=True)
     llm_response = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     metadata = models.JSONField(null=True)
+    source = models.CharField(max_length=255, null=True)
 
     def __str__(self) -> str:
         return f"{self.message}"
