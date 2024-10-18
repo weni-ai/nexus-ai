4d0345f35b882acf90daf32cbd4a80690ca2a8bc
Mardone <mardone.dev@gmail.com>
2024-10-18 18:10:30 -0300

    Change uuid field on action crud

--- nexus/actions/api/serializers.py
+++ nexus/actions/api/serializers.py
@@ -9,7 +9,7 @@ from nexus.actions.models import (
 class FlowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flow
-        fields = ['uuid', 'name', 'prompt', 'fallback', 'content_base', 'action_type', 'group', 'editable']
+        fields = ['uuid', 'name', 'prompt', 'fallback', 'content_base', 'action_type', 'group', 'editable', 'flow_uuid']
 
 
 class TemplateActionSerializer(serializers.ModelSerializer):
