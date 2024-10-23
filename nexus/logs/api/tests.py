a2a09a3f059f532267c7f9ca4af031ab30d0a90a
Mardone <mardone.dev@gmail.com>
2024-10-23 17:46:42 -0300

    fix flake8

--- nexus/logs/api/tests.py
+++ nexus/logs/api/tests.py
@@ -362,4 +362,3 @@ class TagPercentageViewSetTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['error'], "started_day parameter is required")
-
