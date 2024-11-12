f60e1ecd2da2df6ec753d70bf2b2fa029b1c467e
Alisson <alissonfilipe937@gmail.com>
2024-11-12 15:30:04 -0300

    message-detail endpoint append slash

--- nexus/logs/api/routers.py
+++ nexus/logs/api/routers.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('<project_uuid>/activities/', RecentActivitiesViewset.as_view({'get': 'list'}), name='list-activities'),
     path('<project_uuid>/message_history/', MessageHistoryViewset.as_view({'get': 'list'}), name='list-message-history'),
     path('<project_uuid>/tags-analytics/', TagPercentageViewSet.as_view({'get': 'list'}), name='list-tag-percentage'),
-    path('<project_uuid>/message-detail/<log_id>', MessageDetailViewSet.as_view(), name="message-detail"),
+    path('<project_uuid>/message-detail/<log_id>/', MessageDetailViewSet.as_view(), name="message-detail"),
 ]
 
 urlpatterns.append(path('prometheus/', include('django_prometheus.urls')))
