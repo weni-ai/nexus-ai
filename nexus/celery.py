3ab1ec7f9b0099045a8156b72db29e43e0288d15
Alisson <alissonfilipe937@gmail.com>
2024-11-14 12:00:07 -0300

    save

--- nexus/celery.py
+++ nexus/celery.py
@@ -15,9 +15,9 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 task_create_missing_queues = True
 
-app.conf.event_serializer = 'pickle'
-app.conf.task_serializer = 'pickle'
-app.conf.result_serializer = 'pickle'
+app.conf.event_serializer = 'msgpack'
+app.conf.task_serializer = 'msgpack'
+app.conf.result_serializer = 'msgpack'
 app.conf.accept_content = ['application/json', 'application/x-python-serialize']
 
 app.conf.beat_schedule = {
