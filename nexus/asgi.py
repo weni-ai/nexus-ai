146eaedf7ae1b516346afe5e10ca72b3059c7cbc
Alisson <alissonfilipe937@gmail.com>
2024-11-22 10:42:15 -0300

    setup django asgi.py

--- nexus/asgi.py
+++ nexus/asgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexus.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
