d68a489a221a58b80a2199a32450713260c203c9
Alisson <alissonfilipe937@gmail.com>
2024-12-03 12:01:10 -0300

    fix message evaluation

--- nexus/projects/api/views.py
+++ nexus/projects/api/views.py
@@ -8,12 +8,6 @@ from nexus.usecases.projects.update import ProjectUpdateUseCase
 from nexus.usecases.projects.dto import UpdateProjectDTO
 from nexus.usecases.projects.retrieve import get_project
 
-from nexus.usecases.logs.retrieve import RetrieveMessageUseCase
-from nexus.usecases.logs.create import CreateLogUsecase
-
-from nexus.projects.api.serializers import MessageDetailSerializer
-from nexus.projects.api.permissions import ProjectPermission
-
 
 class ProjectUpdateViewset(views.APIView):
     permission_classes = [IsAuthenticated]
@@ -39,32 +33,3 @@ class ProjectUpdateViewset(views.APIView):
         return Response(
             ProjectSerializer(updated_project).data
         )
-
-
-class MessageDetailViewSet(views.APIView):
-    permission_classes = [IsAuthenticated, ProjectPermission]
-
-    def get(self, request, project_uuid, message_uuid):
-        message = RetrieveMessageUseCase().get_by_uuid(message_uuid)
-        return Response(MessageDetailSerializer(message).data)
-
-    def patch(self, request, project_uuid, message_uuid):
-        data = request.data
-        usecase = CreateLogUsecase()
-        usecase.message = RetrieveMessageUseCase().get_by_uuid(message_uuid)
-        usecase.log = usecase.message.messagelog
-
-        serializer = MessageDetailSerializer(usecase.message, data=data, partial=True)
-        serializer.is_valid()
-
-        usecase.update_log_field(**data)
-        keys = list(data.keys())
-        response_data = {}
-
-        for key in keys:
-            response_data.update(
-                {
-                    key: getattr(usecase.log, key)
-                }
-            )
-        return Response(response_data)
