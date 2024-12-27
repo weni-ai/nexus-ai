4d85e88f1d8f4ce8c83e954d0a342584090f3a66
Alisson <alissonfilipe937@gmail.com>
2024-12-27 19:03:27 -0300

    add agents app

---
+++ nexus/agents/api/views.py
@@ -0,0 +1,43 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from nexus.usecases.projects import get_project_by_uuid
+from nexus.usecases.agents import AgentDTO, AgentUsecase
+from nexus.agents.models import Agent
+
+from nexus.projects.api.permissions import ProjectPermission
+
+
+class PushAgents(APIView):
+
+    permission_classes = [IsAuthenticated, ProjectPermission]
+
+    def post(self, request, *args, **kwargs):
+        def get_agents(agents):
+            dto_list = []
+            for agent_slug in agents:
+                agent = agents.get(agent_slug)
+                agent.update({"slug": agent_slug})
+                dto_list.append(AgentDTO(**agent))
+            return dto_list
+
+        project_uuid = request.data.get("project")
+        project = get_project_by_uuid(project_uuid)
+
+        agents = request.data.get("agents")
+        agents_dto = get_agents(agents)
+
+        print(agents_dto)
+
+        agents = []
+
+        for agent_dto in agents_dto:
+            agent: Agent = AgentUsecase().create_agent(agent_dto, project_uuid)
+
+            agents.append({"agent_name": agent.display_name, "agent_external_id": agent.external_id})
+
+        return Response({
+            "project": str(project.uuid),
+            "agents": agents
+        })
---
+++ nexus/agents/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from nexus.projects.models import Project
+from nexus.db.models import BaseModel
+
+
+class Agent(BaseModel):
+    external_id = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    display_name = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    is_official = models.BooleanField(default=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    metadata = models.JSONField(default=dict)
+
+
+class Team(models.Model):
+    external_id = models.CharField(max_length=255, help_text="Supervisor ID")
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+
+
+class ActiveAgent(BaseModel):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    is_official = models.BooleanField(default=False)
