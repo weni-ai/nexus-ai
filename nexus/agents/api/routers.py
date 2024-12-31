bd25a2af926c050e8e085094e3332cac4ca18d18
Mardone <mardone.dev@gmail.com>
2024-12-31 10:51:10 -0300

    New api router

--- nexus/agents/api/routers.py
+++ nexus/agents/api/routers.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from nexus.agents.api.views import PushAgents
+from nexus.agents.api.views import (
+    PushAgents,
+    ActiveAgentsViewSet,
+    TeamViewset
+)
 
 
 urlpatterns = [
     path('agents/push', PushAgents.as_view(), name="push-agents"),
+    path('agents/assign/<str:agent_uuid>', ActiveAgentsViewSet.as_view(), name="assign-agents"),
+    path('agents/teams/<project_uuid>', TeamViewset.as_view(), name="teams"),
 ]
