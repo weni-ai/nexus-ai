45a07e389400572096678f6408bb835407e23f33
Alisson <alissonfilipe937@gmail.com>
2025-01-14 16:18:29 -0300

    remove aws keys

--- nexus/storage.py
+++ nexus/storage.py
@@ -41,8 +41,8 @@ class DeleteStorageFile:
     def __init__(self):
         self.s3_client = boto3.client(
             's3',
-            aws_access_key_id=settings.AWS_S3_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_S3_SECRET_ACCESS_KEY,
+            # aws_access_key_id=settings.AWS_S3_ACCESS_KEY_ID,
+            # aws_secret_access_key=settings.AWS_S3_SECRET_ACCESS_KEY,
             region_name=settings.AWS_S3_REGION_NAME
         )
 
