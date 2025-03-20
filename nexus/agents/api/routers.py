d6d9833a08fdd906988a08f3fc8d3cff09dff1df
Alisson <alissonfilipe937@gmail.com>
2025-03-20 17:09:44 -0300

    Add AgentTracesView and serializers for AgentMessage history
    - Introduced AgentTracesView to handle trace retrieval based on project UUID and log ID.
    - Added AgentMessageHistorySerializer and AgentMessageDetailSerializer for serializing AgentMessage data.
    - Updated the AgentUsecase to include methods for fetching agent messages and traces.
    - Enhanced existing views to utilize the new serializers for improved message handling.

--- nexus/agents/api/routers.py
+++ nexus/agents/api/routers.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from nexus.agents.api.views import (
     ActiveAgentsViewSet,
+    AgentTracesView,
     AgentsView,
     PushAgents,
     TeamView,
@@ -14,6 +15,7 @@ urlpatterns = [
     path('agents/teams/<project_uuid>', TeamView.as_view(), name="teams"),
     path('agents/my-agents/<project_uuid>', AgentsView.as_view(), name="my-agents"),
     path('agents/official/<project_uuid>', OfficialAgentsView.as_view(), name="official-agents"),
+    path('agents/traces/', AgentTracesView.as_view(), name="traces"),
     path('project/<project_uuid>/assign/<str:agent_uuid>', ActiveAgentsViewSet.as_view(), name="assign-agents"),
     path('project/<project_uuid>/credentials', ProjectCredentialsView.as_view(), name="project-credentials"),
     path('project/<project_uuid>/credentials/<str:agent_uuid>', ProjectCredentialsView.as_view(), name="project-credentials-update"),
