e6f3b096829eb95e8bdeb30b62f1d6960273fe11
sandro.meireles <sandro.meirelesdev@gmail.com>
2025-04-01 17:41:47 -0300

    feat: Implement BedrockTeamAdapter and enhance BedrockBackend to invoke agents with external team data

--- inline_agents/backends/bedrock/adapter.py
+++ inline_agents/backends/bedrock/adapter.py
@@ -5,4 +5,34 @@ from inline_agents.team import Team
 class BedrockTeamAdapter(TeamAdapter):
     @classmethod
     def to_external(self, team: Team) -> dict:
+        external_team = {
+            "actionGroups": self._get_action_groups(team),
+            "instruction": self._get_instruction(team),
+            "foundationModel": team.foundation_model,
+            "sessionId": team.session_id,
+            "agentCollaboration": team.agent_collaboration,
+            "inputText": team.input_text,
+            "inlineSessionState": team.inline_session_state,
+            "knowledgeBases": self._get_knowledge_bases(team),
+            "promptOverrideConfiguration": team.prompt_override_configuration,
+            "enableTrace": team.enable_trace,
+            "collaborators": self._get_collaborators(team),
+            "collaboratorConfigurations": self._get_collaborator_configurations(team),
+        }
+
+        return external_team
+
+    def _get_action_groups(self, team: Team) -> list:
+        pass
+
+    def _get_instruction(self, team: Team) -> str:
+        pass
+
+    def _get_foundation_model(self, team: Team) -> str:
+        pass
+
+    def _get_session_id(self, team: Team) -> str:
+        pass
+
+    def _get_agent_collaboration(self, team: Team) -> str:
         pass
--- inline_agents/backends/bedrock/backend.py
+++ inline_agents/backends/bedrock/backend.py
@@ -1,11 +1,26 @@
+import boto3
+
 from inline_agents.backend import InlineAgentsBackend
-from inline_agents.adapter import TeamAdapter
 from inline_agents.team import Team
+from nexus.environment import env
+
+from .adapter import BedrockTeamAdapter
 
 
 class BedrockBackend(InlineAgentsBackend):
-    def invoke_agents(self, team: Team, team_adapter: TeamAdapter):
-        external_team = team_adapter.to_external(team)
+
+    team_adapter = BedrockTeamAdapter
+
+    REGION_NAME = env.str('AWS_BEDROCK_REGION_NAME')
+
+    def _get_client(self):
+        return boto3.client('bedrock-agent-runtime', region_name=self.REGION_NAME)
+
+    def invoke_agents(self, team: Team):
+        external_team = self.team_adapter.to_external(team)
+        client = self._get_client()
+
+        response = client.invoke_inline_agent(**external_team)
 
         # TODO: Invoke inline agent
         # TODO: Implement observer to traces
