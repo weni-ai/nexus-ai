c1ede3ef6b0048c034ac0872ce2cae3192da3bfd
Alisson <alissonfilipe937@gmail.com>
2025-05-19 16:39:51 -0300

    Add ProjectPermission to ReportView for enhanced access control

--- nexus/reports/views.py
+++ nexus/reports/views.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from rest_framework import views
 from rest_framework.response import Response
 
+from nexus.projects.api.permissions import ProjectPermission
 from nexus.task_managers.tasks import generate_flows_report
-from django.conf import settings
 
 
 class ReportView(views.APIView):
+    permission_classes = [ProjectPermission]
     def get(self, request):
         return Response({
             "emails": settings.REPORT_RECIPIENT_EMAILS
