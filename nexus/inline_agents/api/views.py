f33cdbcace4dc0550f3026cdcb857dae8f01c3c2
Jackson Barbosa <jacksonbarbosa321@gmail.com>
2025-06-16 10:43:35 -0300

    adding checking for multi agents API to check the valid emails

--- nexus/inline_agents/api/views.py
+++ nexus/inline_agents/api/views.py
@@ -448,7 +448,7 @@ class MultiAgentView(APIView):
             project = Project.objects.get(uuid=project_uuid)
             return Response({
                 "multi_agents": project.inline_agent_switch,
-                "can_view": (("@weni.ai" in request.user.email) or ("@vtex.com" in request.user.email))
+                "can_view": can_view
             })
         except Project.DoesNotExist:
             return Response(
