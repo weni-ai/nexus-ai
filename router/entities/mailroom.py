97b5be3a0084f4e2333c82225132fdfcf34bcc0f
Alisson <alissonfilipe937@gmail.com>
2025-07-17 10:23:58 -0300

    remove reduntant operations; add edge cases to tests

--- router/entities/mailroom.py
+++ router/entities/mailroom.py
@@ -34,23 +34,20 @@ class Message(BaseModel):
 
     @property
     def sanitized_urn(self):
+        urn_to_sanitize = self.contact_urn
+
+        pattern = r'(:[0-9]+)@.*'
+        match = re.search(pattern, urn_to_sanitize)
+        if match:
+            urn_to_sanitize = re.sub(pattern, r'\1', urn_to_sanitize)
+
         sanitized = ""
-        self.sanitize_webchat_urn()
-        for char in self.contact_urn:
+        for char in urn_to_sanitize:
             if not char.isalnum() and char not in '-_.:':
                 sanitized += f"_{ord(char)}"
             else:
                 sanitized += char
         return sanitized
-    
-    def sanitize_webchat_urn(self):
-        pattern = r'(:[0-9]+)@'
-        match = re.search(pattern, self.contact_urn)
-        if match:
-            colon_number_end = match.end(1)
-            at_position = self.contact_urn.find('@', colon_number_end)
-            if at_position != -1:
-                self.contact_urn = self.contact_urn[:at_position]
 
 
 def message_factory(*args, contact_fields: dict = {}, metadata: dict = {}, **kwargs) -> Message:
