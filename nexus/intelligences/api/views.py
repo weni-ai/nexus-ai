e94f1c4fe1e64c6f88dffd078874d0eea97073d9
Mardone <mardone.dev@gmail.com>
2025-07-21 12:04:11 -0300

    missing kwargs on create

--- nexus/intelligences/api/views.py
+++ nexus/intelligences/api/views.py
@@ -748,7 +748,7 @@ class InlineContentBaseFileViewset(ModelViewSet):
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
-    def create(self, request):
+    def create(self, request, **kwargs):
         from rest_framework import status as http_status
 
         def validate_file_size(file):
