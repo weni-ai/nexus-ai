cc9092379e14c080959d90e820eb4ac5649fa52b
RuanHeleno <ruan.empreenderop@gmail.com>
2025-11-25 11:23:29 -0300

    change drf-yasg for drf-spectacular

--- nexus/urls.py
+++ nexus/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 from nexus.actions.api.routers import urlpatterns as actions_routes
 from nexus.agents.api.routers import urlpatterns as agent_routes
@@ -13,16 +15,6 @@ from nexus.logs.api.routers import urlpatterns as logs_routes
 from nexus.projects.api.routers import urlpatterns as projects_routes
 from nexus.users.api.urls import urlpatterns as users_routes
 from nexus.zeroshot.api.routers import urlpatterns as zeroshot_routes
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Documentation",
-        default_version="v1.0.0",
-        license=openapi.License(name="GPL-3.0 License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 url_api = []
 
@@ -36,7 +28,9 @@ url_api += analytics_routes
 
 urlpatterns = [
     path("", lambda _: HttpResponse()),
-    path("docs/", schema_view.with_ui("redoc")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("docs/", SpectacularRedocView.as_view(url_name="schema")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(url_api)),
