77b96068bff08fb99049e10322d6e006a0cda78d
RuanHeleno <ruan.empreenderop@gmail.com>
2026-04-09 16:15:15 -0300

    Add timestamp to project type update messages using UTC format

--- nexus/usecases/projects/project_type_update_eda.py
+++ nexus/usecases/projects/project_type_update_eda.py
@@ -1,7 +1,13 @@
+import pendulum
+
 from nexus.event_driven.publisher.rabbitmq_publisher import RabbitMQPublisher
 
 UPDATE_PROJECTS_EXCHANGE = "update-projects.topic"
 PROJECT_TYPE_UPDATE_ACTION = "project_type_update"
+
+
+def _utc_timestamp_z() -> str:
+    return pendulum.now("UTC").to_iso8601_string()
 
 
 def publish_project_type_update(*, project_uuid: str, user_email: str, is_multi_agents: bool) -> None:
@@ -11,5 +17,6 @@ def publish_project_type_update(*, project_uuid: str, user_email: str, is_multi_
         "action": PROJECT_TYPE_UPDATE_ACTION,
         "user_email": user_email or "",
         "is_multi_agents": is_multi_agents,
+        "timestamp": _utc_timestamp_z(),
     }
     publisher.send_message(body=body, exchange=UPDATE_PROJECTS_EXCHANGE, routing_key="")
