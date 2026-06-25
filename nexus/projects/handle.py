43260bbae5741fe73e1478cbdf72f726744b796c
mardone.silva@dev.com <mardone.silva@vtex.com>
2026-06-25 14:24:47 -0300

    feat: migrate project consumer to Amazon MQ via weni-eda

--- nexus/projects/handle.py
+++ nexus/projects/handle.py
@@ -1,7 +1,7 @@
 from amqp.channel import Channel
 
-from .consumers.flow_consumer import FlowConsumer
 from .consumers.channel_wwc_consumer import ChannelWwcConsumer
+from .consumers.flow_consumer import FlowConsumer
 from .consumers.integrated_feature import (
     CreateIntegratedFeatureConsumer,
     DeleteIntegratedFeatureConsumer,
@@ -9,13 +9,13 @@ from .consumers.integrated_feature import (
     UpdateIntegratedFeatureConsumer,
 )
 from .consumers.project_auth_consumer import ProjectAuthConsumer
-from .consumers.project_consumer import ProjectConsumer
+from .consumers.project_consumer import OldProjectConsumer
 
 
 def handle_consumers(channel: Channel) -> None:
     # Queue must be created in RabbitMQ and bound to exchange channel-events.topic with routing key wwc-create.
     channel.basic_consume("nexus-ai.channel.wwc-create", callback=ChannelWwcConsumer().handle)
-    channel.basic_consume("nexus-ai.projects", callback=ProjectConsumer().handle)
+    channel.basic_consume("nexus-ai.projects", callback=OldProjectConsumer().handle)
     channel.basic_consume("recent-activity.nexus", callback=FlowConsumer().handle)
     channel.basic_consume("nexus-ai.projects.auth", callback=ProjectAuthConsumer().handle)
     channel.basic_consume("nexus.integrated-feature", callback=CreateIntegratedFeatureConsumer().handle)
