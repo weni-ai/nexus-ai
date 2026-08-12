5fa67ac4ae3f4d76b8f99cc741e0f83666d2fb7b
Alisson <alissonfilipe937@gmail.com>
2026-08-12 15:22:44 -0300

    Revert "Merge branch 'feat/project-vtex-config' into staging"
    This reverts commit faa318eb51d3b7df20395789e99af6de70e86606, reversing
    changes made to 81ff23665dc2a589a7c0761064c4f90cef7fca6b.

--- nexus/projects/consumers/project_consumer.py
+++ nexus/projects/consumers/project_consumer.py
@@ -1,7 +1,6 @@
 import logging
 
 import amqp
-from django.db import IntegrityError, transaction
 from sentry_sdk import capture_exception
 from weni.eda.django.consumers import EDAConsumer as WeniEDAConsumer
 from weni.eda.messages import Message as WeniMessage
@@ -11,11 +10,6 @@ from nexus.event_driven.parsers import JSONParser
 from nexus.projects.models import Project
 from nexus.projects.project_dto import ProjectCreationDTO
 from nexus.usecases.projects.projects_use_case import ProjectsUseCase
-from nexus.usecases.projects.sync_vtex import (
-    SyncProjectVtexUseCase,
-    extract_vtex_fields,
-    unwrap_eda_payload,
-)
 
 logger = logging.getLogger(__name__)
 
@@ -43,36 +37,14 @@ class OldProjectConsumer(EDAConsumer):
             extra={"body_len": len(message.body) if hasattr(message, "body") else None},
         )
         try:
-            body = unwrap_eda_payload(JSONParser.parse(message.body))
-            project_uuid = body.get("uuid")
-            vtex_fields = extract_vtex_fields(body)
+            body = JSONParser.parse(message.body)
+            project_dto = _build_project_dto(body)
 
-            project_dto = ProjectCreationDTO(
-                uuid=project_uuid,
-                name=body.get("name"),
-                is_template=body.get("is_template"),
-                template_type_uuid=body.get("template_type_uuid"),
-                org_uuid=body.get("organization_uuid"),
-                brain_on=body.get("brain_on"),
-                authorizations=body.get("authorizations"),
-                indexer_database=body.get("indexer_database") or Project.BEDROCK,
-                inline_agent_switch=body.get("inline_agent_switch", True),
-            )
-
-            try:
-                with transaction.atomic():
-                    ProjectsUseCase().create_project(project_dto=project_dto, user_email=body.get("user_email"))
-                logger.info("[ProjectConsumer] Project created", extra={"uuid": project_uuid})
-            except IntegrityError:
-                # Reprocessing the same create message must not duplicate the project
-                logger.info(
-                    "[ProjectConsumer] Project already exists, syncing VTEX fields only",
-                    extra={"uuid": project_uuid},
-                )
-
-            SyncProjectVtexUseCase().sync_project_vtex(project_uuid, vtex_fields, mode="create")
+            project_creation = ProjectsUseCase()
+            project_creation.create_project(project_dto=project_dto, user_email=body.get("user_email"))
 
             message.channel.basic_ack(message.delivery_tag)
+            logger.info("[OldProjectConsumer] Project created", extra={"uuid": project_dto.uuid})
         except Exception as exception:
             capture_exception(exception)
             message.channel.basic_reject(message.delivery_tag, requeue=False)
